crdroidandroid/android_frameworks_base

SystemUI: DynamicBar: Align expanded cards to top center
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/AxDynamicBarExpandedPanel.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.input.pointer.PointerEventPass
@@ -273,11 +272,6 @@ private fun OverlayContent(viewModel: AxDynamicBarChipViewModel, statusBarHeight
 
     val originX = chipX
     val origin = TransformOrigin(originX, 0f)
-    
-    val chipAlignment = BiasAlignment(
-        horizontalBias = originX * 2f - 1f,  
-        verticalBias = -1f,                    
-    )
 
     AnimatedVisibility(
         visibleState = expandedVisible,
@@ -325,7 +319,7 @@ private fun OverlayContent(viewModel: AxDynamicBarChipViewModel, statusBarHeight
                     }
                 }
                 .padding(top = topPad),
-            contentAlignment = chipAlignment,
+            contentAlignment = Alignment.TopCenter,
         ) {
             chipState?.let { state ->
                 ExpandedIslandContent(
@@ -339,24 +333,26 @@ private fun OverlayContent(viewModel: AxDynamicBarChipViewModel, statusBarHeight
         }
     }
 
+    val alertOrigin = TransformOrigin(0.5f, 0f)
+
     AnimatedVisibility(
         visibleState = notifVisible,
         enter = fadeIn(tween(300)) + scaleIn(
             animationSpec = tween(300),
             initialScale = 0.4f,
-            transformOrigin = origin,
+            transformOrigin = alertOrigin,
         ),
         exit = fadeOut(tween(250)) + scaleOut(
             animationSpec = tween(250),
             targetScale = 0.4f,
-            transformOrigin = origin,
+            transformOrigin = alertOrigin,
         ),
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = topPad),
-            contentAlignment = chipAlignment,
+            contentAlignment = Alignment.TopCenter,
         ) {
             val alert = lastAlert.value
             if (alert != null) {
@@ -393,4 +389,3 @@ private class PanelLifecycleOwner : LifecycleOwner, SavedStateRegistryOwner {
         lifecycleRegistry.handleLifecycleEvent(event)
     }
 }
-

anonytry/sakura_fw

Signed-off-by: TwistedVision518 <pranavpraveen0@gmail.com>
Signed-off-by: Ghosuto <clash.raja10@gmail.com>
Signed-off-by: TopexGuy <topexguy@gmail.com>

packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/AxDynamicBarExpandedPanel.kt
@@ -21,7 +21,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.input.pointer.PointerEventPass
@@ -241,7 +240,6 @@ private fun OverlayContent(viewModel: AxDynamicBarChipViewModel, statusBarHeight
         else with(density) { statusBarHeightPx.toDp() } + largeScreenExtra
     val chipState by viewModel.chipState.collectAsStateWithLifecycle()
     val isExpanded by viewModel.isExpanded.collectAsStateWithLifecycle()
-    val chipX by viewModel.chipCenterXFraction.collectAsStateWithLifecycle()
 
     val expandedVisible = remember { MutableTransitionState(false) }
 
@@ -256,13 +254,7 @@ private fun OverlayContent(viewModel: AxDynamicBarChipViewModel, statusBarHeight
         }
     }
 
-    val originX = chipX
-    val origin = TransformOrigin(originX, 0f)
-    
-    val chipAlignment = BiasAlignment(
-        horizontalBias = originX * 2f - 1f,  
-        verticalBias = -1f,                    
-    )
+    val origin = TransformOrigin(0.5f, 0f)
 
     AnimatedVisibility(
         visibleState = expandedVisible,
@@ -310,7 +302,7 @@ private fun OverlayContent(viewModel: AxDynamicBarChipViewModel, statusBarHeight
                     }
                 }
                 .padding(top = topPad),
-            contentAlignment = chipAlignment,
+            contentAlignment = Alignment.TopCenter,
         ) {
             chipState?.let { state ->
                 val filtered = state.allEvents.filter { it !is IslandEvent.AospChip }